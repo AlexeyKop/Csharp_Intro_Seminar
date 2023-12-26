6246a609cf44443744d74d67f4b175939ba9cd8e
AlexeyKop <a.kop@bk.ru>
2023-12-27 00:55:25 +0400

    Создали метод для генерации случайного массива строк. Строка тоже генерируется случайным образом в диапазоне от 'A' до 'я' в кол-ве от 1 до 7 символов

--- DZ_Csharp/DZ8_Total/Program.cs
+++ DZ_Csharp/DZ8_Total/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Метод для генерации случайного массива строк
+string[] GenerateRandomArray(int minLength, int maxLength)
+{
+    Random random = new Random();
+    int length = random.Next(minLength, maxLength);
+
+    string[] array = new string[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        int stringLength = random.Next(1, 7);
+        char[] chars = new char[stringLength];
+
+        // Генерация случайной строки
+        for (int j = 0; j < stringLength; j++)
+        {
+            chars[j] = (char)random.Next('А', 'я');
+        }
+
+        array[i] = new string(chars);
+    }
+
+    return array;
+}
